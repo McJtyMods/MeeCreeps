44be0bd2203321fffe5ff3384b95c07fe319efb3
McJty <mcjty1@gmail.com>
2017-10-28 19:57:49 +0200

    Oops. Too many spawns!

--- src/main/java/mcjty/meecreeps/entities/ModEntities.java
+++ src/main/java/mcjty/meecreeps/entities/ModEntities.java
@@ -1,8 +1,6 @@
 package mcjty.meecreeps.entities;
 
 import mcjty.meecreeps.MeeCreeps;
-import net.minecraft.entity.EnumCreatureType;
-import net.minecraft.init.Biomes;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.storage.loot.LootTableList;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
@@ -16,10 +14,6 @@ public class ModEntities {
         // Every entity in our mod has an ID (local to this mod)
         int id = 1;
         EntityRegistry.registerModEntity(new ResourceLocation(MeeCreeps.MODID, "meecreeps"), EntityMeeCreeps.class, "MeeCreeps", id++, MeeCreeps.instance, 64, 3, true, 0x996600, 0x00ff00);
-
-        // We want our mob to spawn in Plains and ice plains biomes. If you don't add this then it will not spawn automatically
-        // but you can of course still make it spawn manually
-        EntityRegistry.addSpawn(EntityMeeCreeps.class, 100, 3, 5, EnumCreatureType.MONSTER, Biomes.PLAINS, Biomes.ICE_PLAINS);
 
         // This is the loot table for our mob
         LootTableList.register(EntityMeeCreeps.LOOT);
