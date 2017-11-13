64529b2236cd880d0e9696ebfe8e1448830d66c5
wiiv <wiivwork@gmail.com>
2017-11-13 19:53:33 +0000

    Hairy situation.

--- src/main/java/mcjty/meecreeps/entities/MeeCreepsModel.java
+++ src/main/java/mcjty/meecreeps/entities/MeeCreepsModel.java
@@ -75,7 +75,7 @@ public class MeeCreepsModel extends ModelBiped {
         }
         
         for (int i = 0 ; i < 9 ; i++) {
-            this.hairVariation[i] = new ModelRenderer(this, 36, getTextureV(i));
+            this.hairVariation[i] = new ModelRenderer(this, 36, 0);
             this.hairVariation[i].setRotationPoint(0.0F, -8.0F, 0.0F);
             this.hairVariation[i].addBox(-1.0F, 0.5F, -1.0F, 2, 2, 2, 0.0F);
         }
@@ -103,161 +103,161 @@ public class MeeCreepsModel extends ModelBiped {
         this.bipedLeftLeg.addBox(-1.0F, 0.0F, -1.0F, 2, 16, 2, 0.0F);
         
         this.hair_01 = new ModelRenderer(this, 40, 18);
-        this.hair_01.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.hair_01.setRotationPoint(0.0F, -10.0F, 0.0F);
         this.hair_01.addBox(0.0F, -3.0F, -1.0F, 1, 3, 1, 0.0F);
         this.setRotateAngle(hair_01, 0.5235987755982988F, 0.0F, 1.3089969389957472F);
         this.hair_02 = new ModelRenderer(this, 40, 18);
-        this.hair_02.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.hair_02.setRotationPoint(0.0F, -10.0F, 0.0F);
         this.hair_02.addBox(0.0F, -3.0F, -0.5F, 1, 3, 1, 0.0F);
         this.setRotateAngle(hair_02, 0.0F, 0.0F, 1.0471975511965976F);
         this.hair_03 = new ModelRenderer(this, 40, 18);
-        this.hair_03.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.hair_03.setRotationPoint(0.0F, -10.0F, 0.0F);
         this.hair_03.addBox(0.0F, -3.0F, 0.0F, 1, 3, 1, 0.0F);
         this.setRotateAngle(hair_03, -0.5235987755982988F, 0.0F, 1.3089969389957472F);
         this.hair_04 = new ModelRenderer(this, 40, 18);
-        this.hair_04.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.hair_04.setRotationPoint(0.0F, -10.0F, 0.0F);
         this.hair_04.addBox(-1.0F, -3.0F, -1.0F, 1, 3, 1, 0.0F);
         this.setRotateAngle(hair_04, 0.5235987755982988F, 0.0F, -1.3089969389957472F);
         this.hair_05 = new ModelRenderer(this, 40, 18);
-        this.hair_05.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.hair_05.setRotationPoint(0.0F, -10.0F, 0.0F);
         this.hair_05.addBox(-1.0F, -3.0F, -0.5F, 1, 3, 1, 0.0F);
         this.setRotateAngle(hair_05, 0.0F, 0.0F, -1.0471975511965976F);
         this.hair_06 = new ModelRenderer(this, 40, 18);
-        this.hair_06.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.hair_06.setRotationPoint(0.0F, -10.0F, 0.0F);
         this.hair_06.addBox(-1.0F, -3.0F, 0.0F, 1, 3, 1, 0.0F);
         this.setRotateAngle(hair_06, -0.5235987755982988F, 0.0F, -1.3089969389957472F);
         
         this.hair_11 = new ModelRenderer(this, 40, 18);
-        this.hair_11.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.hair_11.setRotationPoint(0.0F, -10.0F, 0.0F);
         this.hair_11.addBox(-1.0F, -3.0F, -0.5F, 1, 3, 1, 0.0F);
         this.setRotateAngle(hair_11, 0.0F, 0.0F, -0.5235987755982988F);
         this.hair_12 = new ModelRenderer(this, 40, 18);
-        this.hair_12.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.hair_12.setRotationPoint(0.0F, -10.0F, 0.0F);
         this.hair_12.addBox(-1.0F, -3.0F, -0.5F, 1, 3, 1, 0.0F);
         this.setRotateAngle(hair_12, 0.0F, 3.141592653589793F, 0.5235987755982988F);
         this.hair_13 = new ModelRenderer(this, 40, 18);
-        this.hair_13.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.hair_13.setRotationPoint(0.0F, -10.0F, 0.0F);
         this.hair_13.addBox(-0.5F, -3.0F, -1.0F, 1, 3, 1, 0.0F);
         this.hair_14 = new ModelRenderer(this, 40, 18);
-        this.hair_14.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.hair_14.setRotationPoint(0.0F, -10.0F, 0.0F);
         this.hair_14.addBox(-0.5F, -3.0F, 0.0F, 1, 3, 1, 0.0F);
         this.setRotateAngle(hair_14, -0.5235987755982988F, 0.0F, 0.0F);
         
         this.hair_21 = new ModelRenderer(this, 40, 18);
-        this.hair_21.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.hair_21.setRotationPoint(0.0F, -10.0F, 0.0F);
         this.hair_21.addBox(-1.0F, -3.0F, -0.5F, 1, 3, 1, 0.0F);
         this.setRotateAngle(hair_21, 0.7853981633974483F, 0.0F, -0.5235987755982988F);
         this.hair_22 = new ModelRenderer(this, 40, 18);
-        this.hair_22.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.hair_22.setRotationPoint(0.0F, -10.0F, 0.0F);
         this.hair_22.addBox(-1.0F, -3.0F, -0.5F, 1, 3, 1, 0.0F);
         this.setRotateAngle(hair_22, 0.7853981633974483F, 3.141592653589793F, 0.5235987755982988F);
         this.hair_23 = new ModelRenderer(this, 40, 18);
-        this.hair_23.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.hair_23.setRotationPoint(0.0F, -10.0F, 0.0F);
         this.hair_23.addBox(-0.5F, -3.0F, -1.0F, 1, 3, 1, 0.0F);
         this.setRotateAngle(hair_23, 1.0471975511965976F, 0.0F, 0.0F);
         this.hair_24 = new ModelRenderer(this, 40, 18);
-        this.hair_24.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.hair_24.setRotationPoint(0.0F, -10.0F, 0.0F);
         this.hair_24.addBox(-0.5F, -3.0F, 0.0F, 1, 3, 1, 0.0F);
         this.setRotateAngle(hair_24, -1.0471975511965976F, 0.0F, 0.0F);
         
         this.hair_31 = new ModelRenderer(this, 40, 18);
-        this.hair_31.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.hair_31.setRotationPoint(0.0F, -10.0F, 0.0F);
         this.hair_31.addBox(0.0F, -3.0F, -1.0F, 1, 3, 1, 0.0F);
         this.setRotateAngle(hair_31, 1.3089969389957472F, 0.0F, 0.0F);
         this.hair_32 = new ModelRenderer(this, 40, 18);
-        this.hair_32.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.hair_32.setRotationPoint(0.0F, -10.0F, 0.0F);
         this.hair_32.addBox(0.0F, -3.0F, -0.5F, 1, 3, 1, 0.0F);
         this.setRotateAngle(hair_32, 0.2617993877991494F, 0.0F, 0.2617993877991494F);
         this.hair_33 = new ModelRenderer(this, 40, 18);
-        this.hair_33.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.hair_33.setRotationPoint(0.0F, -10.0F, 0.0F);
         this.hair_33.addBox(0.0F, -3.0F, 0.0F, 1, 3, 1, 0.0F);
         this.setRotateAngle(hair_33, -0.7853981633974483F, 0.0F, 0.0F);
         this.hair_34 = new ModelRenderer(this, 40, 18);
-        this.hair_34.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.hair_34.setRotationPoint(0.0F, -10.0F, 0.0F);
         this.hair_34.addBox(-1.0F, -3.0F, -1.0F, 1, 3, 1, 0.0F);
         this.setRotateAngle(hair_34, 0.7853981633974483F, 0.0F, 0.0F);
         this.hair_35 = new ModelRenderer(this, 40, 18);
-        this.hair_35.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.hair_35.setRotationPoint(0.0F, -10.0F, 0.0F);
         this.hair_35.addBox(-1.0F, -3.0F, -0.5F, 1, 3, 1, 0.0F);
         this.setRotateAngle(hair_35, -0.2617993877991494F, 0.0F, -0.2617993877991494F);
         this.hair_36 = new ModelRenderer(this, 40, 18);
-        this.hair_36.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.hair_36.setRotationPoint(0.0F, -10.0F, 0.0F);
         this.hair_36.addBox(-1.0F, -3.0F, 0.0F, 1, 3, 1, 0.0F);
         this.setRotateAngle(hair_36, -1.3089969389957472F, 0.0F, 0.0F);
         
         this.hair_41 = new ModelRenderer(this, 40, 18);
-        this.hair_41.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.hair_41.setRotationPoint(0.0F, -10.0F, 0.0F);
         this.hair_41.addBox(0.0F, -3.0F, -1.0F, 1, 3, 1, 0.0F);
         this.setRotateAngle(hair_41, 1.3089969389957472F, -0.7853981633974483F, 0.0F);
         this.hair_42 = new ModelRenderer(this, 40, 18);
-        this.hair_42.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.hair_42.setRotationPoint(0.0F, -10.0F, 0.0F);
         this.hair_42.addBox(-0.5F, -3.0F, -1.0F, 1, 3, 1, 0.0F);
         this.setRotateAngle(hair_42, 1.0471975511965976F, 0.5235987755982988F, 0.0F);
         this.hair_43 = new ModelRenderer(this, 40, 18);
-        this.hair_43.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.hair_43.setRotationPoint(0.0F, -10.0F, 0.0F);
         this.hair_43.addBox(-1.0F, -3.0F, -1.0F, 1, 3, 1, 0.0F);
         this.setRotateAngle(hair_43, 1.3089969389957472F, 0.7853981633974483F, 0.0F);
         this.hair_44 = new ModelRenderer(this, 40, 18);
-        this.hair_44.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.hair_44.setRotationPoint(0.0F, -10.0F, 0.0F);
         this.hair_44.addBox(-0.5F, -3.0F, -1.0F, 1, 3, 1, 0.0F);
         this.setRotateAngle(hair_44, 1.0471975511965976F, -0.5235987755982988F, 0.0F);
         
         this.hair_51 = new ModelRenderer(this, 40, 18);
-        this.hair_51.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.hair_51.setRotationPoint(0.0F, -10.0F, 0.0F);
         this.hair_51.addBox(-1.0F, -3.0F, -0.5F, 1, 3, 1, 0.0F);
         this.setRotateAngle(hair_51, 1.0471975511965976F, 0.5235987755982988F, 0.0F);
         this.hair_52 = new ModelRenderer(this, 40, 18);
-        this.hair_52.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.hair_52.setRotationPoint(0.0F, -10.0F, 0.0F);
         this.hair_52.addBox(0.0F, -3.0F, -0.5F, 1, 3, 1, 0.0F);
         this.setRotateAngle(hair_52, 1.0471975511965976F, -0.5235987755982988F, 0.0F);
         this.hair_53 = new ModelRenderer(this, 40, 18);
-        this.hair_53.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.hair_53.setRotationPoint(0.0F, -10.0F, 0.0F);
         this.hair_53.addBox(-0.5F, -3.0F, -1.0F, 1, 3, 1, 0.0F);
         this.setRotateAngle(hair_53, 1.3089969389957472F, 0.0F, 0.0F);
         this.hair_54 = new ModelRenderer(this, 40, 18);
-        this.hair_54.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.hair_54.setRotationPoint(0.0F, -10.0F, 0.0F);
         this.hair_54.addBox(-0.5F, -3.0F, 0.0F, 1, 3, 1, 0.0F);
         this.setRotateAngle(hair_54, -0.2617993877991494F, 0.0F, 0.0F);
         
         this.hair_61 = new ModelRenderer(this, 40, 18);
-        this.hair_61.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.hair_61.setRotationPoint(0.0F, -10.0F, 0.0F);
         this.hair_61.addBox(-1.0F, -3.0F, 0.0F, 1, 3, 1, 0.0F);
         this.setRotateAngle(hair_61, -1.0471975511965976F, -0.5235987755982988F, 0.0F);
         this.hair_62 = new ModelRenderer(this, 40, 18);
-        this.hair_62.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.hair_62.setRotationPoint(0.0F, -10.0F, 0.0F);
         this.hair_62.addBox(0.0F, -3.0F, 0.0F, 1, 3, 1, 0.0F);
         this.setRotateAngle(hair_62, -1.0471975511965976F, 0.5235987755982988F, 0.0F);
         this.hair_63 = new ModelRenderer(this, 40, 18);
-        this.hair_63.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.hair_63.setRotationPoint(0.0F, -10.0F, 0.0F);
         this.hair_63.addBox(-0.5F, -3.0F, 0.0F, 1, 3, 1, 0.0F);
         this.setRotateAngle(hair_63, -0.2617993877991494F, 0.0F, 0.0F);
         this.hair_64 = new ModelRenderer(this, 40, 18);
-        this.hair_64.setRotationPoint(5.0F, 5.0F, 0.0F);
+        this.hair_64.setRotationPoint(5.0F, -5.0F, 0.0F);
         this.hair_64.addBox(-0.5F, 0.0F, 0.0F, 1, 3, 1, 0.0F);
         this.setRotateAngle(hair_64, 2.356194490192345F, 0.5235987755982988F, 0.0F);
         this.hair_65 = new ModelRenderer(this, 40, 18);
-        this.hair_65.setRotationPoint(5.0F, 5.0F, 0.0F);
+        this.hair_65.setRotationPoint(5.0F, -5.0F, 0.0F);
         this.hair_65.addBox(-0.5F, 0.0F, -0.5F, 1, 3, 1, 0.0F);
         this.setRotateAngle(hair_65, 1.8325957145940461F, 0.2617993877991494F, 0.0F);
         this.hair_66 = new ModelRenderer(this, 40, 18);
-        this.hair_66.setRotationPoint(5.0F, 5.0F, 0.0F);
+        this.hair_66.setRotationPoint(5.0F, -5.0F, 0.0F);
         this.hair_66.addBox(-0.5F, 0.0F, -1.0F, 1, 3, 1, 0.0F);
         this.setRotateAngle(hair_66, 1.3089969389957472F, 0.2617993877991494F, 0.0F);
         this.hair_67 = new ModelRenderer(this, 40, 18);
-        this.hair_67.setRotationPoint(-5.0F, 5.0F, 0.0F);
+        this.hair_67.setRotationPoint(-5.0F, -5.0F, 0.0F);
         this.hair_67.addBox(-0.5F, 0.0F, 0.0F, 1, 3, 1, 0.0F);
         this.setRotateAngle(hair_67, 2.356194490192345F, -0.5235987755982988F, 0.0F);
         this.hair_68 = new ModelRenderer(this, 40, 18);
-        this.hair_68.setRotationPoint(-5.0F, 5.0F, 0.0F);
+        this.hair_68.setRotationPoint(-5.0F, -5.0F, 0.0F);
         this.hair_68.addBox(-0.5F, 0.0F, -0.5F, 1, 3, 1, 0.0F);
         this.setRotateAngle(hair_68, 1.8325957145940461F, -0.2617993877991494F, 0.0F);
         this.hair_69 = new ModelRenderer(this, 40, 18);
-        this.hair_69.setRotationPoint(-5.0F, 5.0F, 0.0F);
+        this.hair_69.setRotationPoint(-5.0F, -5.0F, 0.0F);
         this.hair_69.addBox(-0.5F, 0.0F, -1.0F, 1, 3, 1, 0.0F);
         this.setRotateAngle(hair_69, 1.3089969389957472F, -0.2617993877991494F, 0.0F);
         
         this.hair_71 = new ModelRenderer(this, 40, 18);
-        this.hair_71.setRotationPoint(0.0F, 1.0F, -5.0F);
+        this.hair_71.setRotationPoint(0.0F, -9.0F, -5.0F);
         this.hair_71.addBox(-0.5F, -3.0F, -1.0F, 1, 3, 1, 0.0F);
         this.setRotateAngle(hair_71, -0.2617993877991494F, 0.0F, 0.2617993877991494F);
         
@@ -364,6 +364,11 @@ public class MeeCreepsModel extends ModelBiped {
         this.bipedLeftArm.rotateAngleX = (float)((double)this.bipedLeftArm.rotateAngleX * 0.5D);
         this.bipedRightLeg.rotateAngleX = (float)((double)this.bipedRightLeg.rotateAngleX * 0.5D);
         this.bipedLeftLeg.rotateAngleX = (float)((double)this.bipedLeftLeg.rotateAngleX * 0.5D);
+        
+        for (int i = 0 ; i < 9 ; i++) {
+        	this.hairVariation[i].rotationPointY = -8.0F;
+        }
+        	
         float f1 = 0.4F;
 
         if (this.bipedRightArm.rotateAngleX > 0.4F)
@@ -422,7 +427,7 @@ public class MeeCreepsModel extends ModelBiped {
         this.bipedLeftLeg.rotationPointZ = 0.0F;
         this.bipedRightLeg.rotationPointY = 8.0F;
         this.bipedLeftLeg.rotationPointY = 8.0F;
-        this.bipedHead.rotationPointZ = -0.0F;
+        this.bipedHead.rotationPointZ = 0.0F;
         this.bipedHead.rotationPointY = -6.0F;
         this.bipedHeadwear.rotationPointX = this.bipedHead.rotationPointX;
         this.bipedHeadwear.rotationPointY = this.bipedHead.rotationPointY;
