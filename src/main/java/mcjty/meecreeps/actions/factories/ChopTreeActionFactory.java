1e4b00c2ffec4486519dc5bfacfee32c935d274b
McJty <mcjty1@gmail.com>
2017-11-14 15:47:55 +0100

    The chop trees action should work on modded trees too

--- src/main/java/mcjty/meecreeps/actions/factories/ChopTreeActionFactory.java
+++ src/main/java/mcjty/meecreeps/actions/factories/ChopTreeActionFactory.java
@@ -4,8 +4,8 @@ import mcjty.meecreeps.actions.workers.ChopTreeActionWorker;
 import mcjty.meecreeps.api.IActionFactory;
 import mcjty.meecreeps.api.IActionWorker;
 import mcjty.meecreeps.api.IWorkerHelper;
+import net.minecraft.block.BlockLog;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.init.Blocks;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -17,7 +17,7 @@ public class ChopTreeActionFactory implements IActionFactory {
     @Override
     public boolean isPossible(World world, BlockPos pos, EnumFacing side) {
         IBlockState state = world.getBlockState(pos);
-        if (state.getBlock() == Blocks.LOG || state.getBlock() == Blocks.LOG2) {
+        if (state.getBlock() instanceof BlockLog) {
             return true;
         }
         return false;
