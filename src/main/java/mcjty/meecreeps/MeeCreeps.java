165e1a23f356e4a88d71018b745fbc1579249f0c
McJty <mcjty1@gmail.com>
2017-12-07 12:00:19 +0100

    1.0.0

--- src/main/java/mcjty/meecreeps/MeeCreeps.java
+++ src/main/java/mcjty/meecreeps/MeeCreeps.java
@@ -24,7 +24,7 @@ import java.util.function.Function;
         acceptedMinecraftVersions = "[1.12,1.13)")
 public class MeeCreeps {
     public static final String MODID = "meecreeps";
-    public static final String VERSION = "0.1.8beta";
+    public static final String VERSION = "1.0.0";
     public static final String MIN_FORGE_VER = "14.22.0.2464";
 
     @SidedProxy(clientSide = "mcjty.meecreeps.proxy.ClientProxy", serverSide = "mcjty.meecreeps.proxy.ServerProxy")
